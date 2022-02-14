4de16d7acee8f2a5433e979c33cb43abc087ba02
Vladimir Kapenkin <vladimir.kapenkin@gmail.com>
2022-02-14 14:06:59 +0300

    PoC - sending email attachments with Gov Notify

---
+++ GovNotifyClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Notify.Client;
+using Notify.Interfaces;
+using Notify.Models.Responses;
+
+namespace GovNotify
+{
+    public class GovNotifyClient
+    {
+        private readonly IAsyncNotificationClient _client;
+
+        private readonly string _emailAddress;
+        private readonly string _templateId;
+        private readonly Dictionary<string, dynamic> _templateValues = new Dictionary<string, dynamic>();
+
+        public GovNotifyClient(IAsyncNotificationClient client)
+        {
+            _client = client;
+        }
+
+        private GovNotifyClient(IAsyncNotificationClient client, string emailAddress, string templateId)
+        {
+            _client = client;
+            _emailAddress = emailAddress;
+            _templateId = templateId;
+        }
+        
+        public GovNotifyClient EmailTo(string emailAddress, string templateId)
+        {
+            return new GovNotifyClient(_client, emailAddress, templateId);
+        }
+
+        public GovNotifyClient WithTemplate(params (string Name, dynamic Value)[] templateValues)
+        {
+            foreach (var (name, value) in templateValues)
+            {
+                _templateValues.TryAdd(name, value);
+            }
+            
+            return this;
+        }
+
+        public GovNotifyClient WithAttachment(string name, string base64String)
+        {
+            return WithAttachment(name, Convert.FromBase64String(base64String));
+        }
+        
+        public GovNotifyClient WithAttachment(string name, byte[] attachmentBytes)
+        {
+            _templateValues.TryAdd(name, NotificationClient.PrepareUpload(attachmentBytes));
+            return this;
+        }
+        
+        public Task<EmailNotificationResponse> SendAsync()
+        {
+            return _client.SendEmailAsync(_emailAddress, _templateId, _templateValues);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Notify.Client;
+using Notify.Models.Responses;
+
+namespace GovNotify
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var apiKey = args[0];
+            var templateId = args[1];;
+            var recipient = args[2];
+
+            var fileContent = "SGVsbG8gd29ybGQh"; // Hello world
+            
+            var response = await SendWithPlainCode(apiKey, templateId, recipient, fileContent);
+            Console.WriteLine(response.content.body);
+
+            response = await SendWithReusableComponent(apiKey, templateId, recipient, fileContent);
+            Console.WriteLine(response.content.body);
+            Console.ReadKey();
+        }
+
+        private static async Task<EmailNotificationResponse> SendWithPlainCode(string apiKey, string templateId, string recipient, string base64Content)
+        {
+            var client = new NotificationClient(apiKey);
+            var documentContents = Convert.FromBase64String(base64Content);
+
+            var templateValues = new Dictionary<string, dynamic>()
+            {
+                {"name", "John Doe"},
+                {"title", "Mr."},
+                {"link1", NotificationClient.PrepareUpload(documentContents)}
+            };
+
+            return await client.SendEmailAsync(recipient, templateId, templateValues);
+        }
+        
+        private static async Task<EmailNotificationResponse> SendWithReusableComponent(string apiKey, string templateId, string recipient, string base64Content)
+        {
+            var notificationClient = new NotificationClient(apiKey);
+            var govNotifyClient = new GovNotifyClient(notificationClient);
+
+            return await govNotifyClient
+                .EmailTo(recipient, templateId)
+                .WithTemplate(
+                    ("name", "John Doe"),
+                    ("title", "Mr."))
+                .WithAttachment("link1", base64Content)
+                .SendAsync();
+        }
+    }
+}
